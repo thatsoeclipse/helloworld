7b84e8d0f756d3e6dcb2acd94e5eb4bfc0037dbb
Eclipse 🍃 <eclipsethatso@gmail.com>
2026-08-20 20:47:02 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-/**
- * Main entry point of the application.
- *
- * Prints standard greeting message to stdout.
- *
- * Example:
- *     $ gcc hello.c -o hello
- *     $ ./hello
- *     Hello,World!
- *
- * Returns:
- *     int: Always 0 on success.
- */
 int main(void) {
     printf("Hello,World!\n");
     return 0;
